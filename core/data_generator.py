b4c5c5567a58082269dee55d013d5a1012f404d3
Jan Huus <75086432+jhuus@users.noreply.github.com>
2022-03-04 09:32:02 -0500

    refactor and tune data augmentation

--- core/data_generator.py
+++ core/data_generator.py
@@ -13,17 +13,22 @@ from core import audio
 from core import constants
 from core import util
 
-MAX_CHANGES = 2            # max changes per spectrogram
-MAX_SHIFT = 5              # max pixels for horizontal shift
-PROB_CHANGES = [0.33, 0.66] # p < P[0] = 0 changes, else p < P[1] = 1 change, else 2 changes
+PROB_AUG = 0.55    # probability of augmentation
+CACHE_LEN = 1000   # cache this many noise specs for performance
 
-# these probabilities affect the relative frequency of the augmentation types
-PROB_BLUR = 0.25
-PROB_FADE = 0.5
-PROB_LOW = 0.25
-PROB_NOISE = 1.0
-PROB_SHIFT = 0.5
-PROB_SPECKLE = 1.0
+MAX_SHIFT = 5      # max pixels for horizontal shift
+NOISE_VARIANCE = 0.0015 # larger variances lead to more noise
+SPECKLE_VARIANCE = .009
+
+# relative frequencies of the augmentation types
+BLUR_INDEX = 0      # so FREQS[0] is relative frequency of blur
+FADE_INDEX = 1
+LOW_INDEX = 2
+NOISE_INDEX = 3
+SHIFT_INDEX = 4
+SPECKLE_INDEX = 5
+
+FREQS = [0.25, 0.5, 0.2, 1.0, 0.5, 0.9]
 
 class DataGenerator():
     def __init__(self, x_train, y_train, seed=None, augmentation=True, binary_classifier=False):
@@ -40,6 +45,15 @@ class DataGenerator():
         
         self.indices = np.arange(y_train.shape[0])
         if self.augmentation:
+            # convert relative frequencies to probability ranges in [0, 1]
+            freqs = np.array(FREQS)
+            sum = np.sum(freqs)
+            probs = freqs / sum
+            self.probs = np.zeros(SPECKLE_INDEX + 1)
+            self.probs[0] = probs[0]
+            for i in range(1, SPECKLE_INDEX + 1):
+                self.probs[i] = self.probs[i - 1] + probs[i]
+
             # self.local_vars is used in _add_low_noise;
             # use exponentially more noise in the low frequencies
             self.local_vars = np.zeros((self.spec_height, constants.SPEC_WIDTH, 1))
@@ -48,6 +62,19 @@ class DataGenerator():
                 for col in range(0, constants.SPEC_WIDTH):
                     self.local_vars[row, col, 0] = np.random.uniform(0.001, max_val)
 
+            # creating these here instead of during augmentation saves a lot of time
+            self.noise = np.zeros((CACHE_LEN, self.spec_height, constants.SPEC_WIDTH, 1))
+            for i in range(CACHE_LEN):
+                self.noise[i] = skimage.util.random_noise(self.noise[i], mode='gaussian', seed=self.seed, var=NOISE_VARIANCE, clip=True)
+
+            self.low_noise = np.zeros((CACHE_LEN, self.spec_height, constants.SPEC_WIDTH, 1))
+            for i in range(CACHE_LEN):
+                self.low_noise[i] = skimage.util.random_noise(self.low_noise[i], mode='localvar', seed=self.seed, local_vars=self.local_vars, clip=True)
+
+            self.speckle = np.zeros((CACHE_LEN, self.spec_height, constants.SPEC_WIDTH, 1))
+            for i in range(CACHE_LEN):
+                self.speckle[i] = skimage.util.random_noise(self.speckle[i], mode='gaussian', seed=self.seed, var=SPECKLE_VARIANCE, clip=True)
+
     # this is called once per epoch to generate the spectrograms
     def __call__(self):
         np.random.shuffle(self.indices)
@@ -55,86 +82,42 @@ class DataGenerator():
             spec = util.expand_spectrogram(self.x_train[id], binary_classifier=self.binary_classifier)
             
             if self.augmentation:
-                # detect left-shifted spectrograms, so we don't shift further left
-                left_part = spec[:self.spec_height, :10]
-                num_pixels = (left_part > 0.05).sum()
-                if num_pixels > 300:
-                    max_shift_left = 0
-                else:
-                    max_shift_left = MAX_SHIFT
-                
-                # detect right-shifted spectrograms, so we don't shift further right
-                right_part = spec[:self.spec_height, constants.SPEC_WIDTH - 10:]
-                num_pixels = (right_part > 0.05).sum()
-                if num_pixels > 300:
-                    max_shift_right = 0
-                else:
-                    max_shift_right = MAX_SHIFT
-
-                # decide the number of changes to apply to this spectrogram
-                changes_todo = MAX_CHANGES
                 prob = np.random.uniform(0, 1)
-                for j in range(MAX_CHANGES):
-                    if prob < PROB_CHANGES[j]:
-                        changes_todo = j
-                        break
-
-                # ensure we don't apply the same change twice
-                done_blur = False
-                done_fade = False
-                done_noise = False # includes regular and low-frequency noise, so we don't apply both to the same spectrogram
-                done_shift = False
-                done_speckle = False
-                
-                # loop, applying changes (data augmentations)
-                changes = ['blur', 'fade', 'low', 'noise', 'shift', 'speckle']
-                changes_done = 0
-                while changes_done < changes_todo:
-                    change_index = random.randint(0, len(changes) - 1) # pick one at random
-                    change = changes[change_index]
+                if prob < PROB_AUG:
                     prob = np.random.uniform(0, 1)
-                    
-                    if change == 'none':
-                        changes_done += 1
-                    elif change == 'blur' and not done_blur and prob < PROB_BLUR:
+
+                    # it's very important to check these in this order
+                    if prob < self.probs[BLUR_INDEX]:
                         spec = self._blur(spec)
-                        done_blur = True
-                        changes_done += 1
-                    elif change == 'fade' and not done_fade and prob < PROB_FADE:
+                    elif prob < self.probs[FADE_INDEX]:
                         spec = self._fade(spec)
-                        done_fade = True
-                        changes_done += 1
-                    elif change == 'low' and not done_noise and prob < PROB_LOW:
+                    elif prob < self.probs[LOW_INDEX]:
                         spec = self._add_low_noise(spec)
-                        done_noise = True
-                        changes_done += 1
-                    elif change == 'noise' and not done_noise and prob < PROB_NOISE:
+                    elif prob < self.probs[NOISE_INDEX]:
                         spec = self._add_noise(spec)
-                        done_noise = True
-                        changes_done += 1
-                    elif change == 'shift' and not done_shift and prob < PROB_SHIFT:
-                        spec = self._shift_horizontal(spec, max_shift_left, max_shift_right)
-                        done_shift = True
-                        changes_done += 1
-                    elif change == 'speckle' and not done_speckle and prob < PROB_SPECKLE:
+                    elif prob < self.probs[SHIFT_INDEX]:
+                        spec = self._shift_horizontal(spec)
+                    else:
                         spec = self._speckle(spec)
-                        done_speckle = True
-                        changes_done += 1
             
             yield (spec.astype(np.float32), self.y_train[id].astype(np.float32))
     
     # add low frequency noise to the spectrogram
     def _add_low_noise(self, spec):
-        spec = skimage.util.random_noise(spec, mode='localvar', seed=self.seed, local_vars=self.local_vars, clip=True)
+        index = random.randint(0, CACHE_LEN - 1)
+        spec += self.low_noise[index]
+        spec = spec.clip(0, 1)
         return spec
 
-    # add random noise to the spectrogram (larger variances lead to more noise)
-    def _add_noise(self, spec, variance=0.001):
-        spec = skimage.util.random_noise(spec, mode='gaussian', seed=self.seed, var=variance, clip=True)
+    # add random noise to the spectrogram
+    def _add_noise(self, spec):
+        index = random.randint(0, CACHE_LEN - 1)
+        spec += self.noise[index]
+        spec = spec.clip(0, 1)
         return spec
 
     # blur the spectrogram (larger values of sigma lead to more blurring)
-    def _blur(self, spec, min_sigma=0.1, max_sigma=1):
+    def _blur(self, spec, min_sigma=0.1, max_sigma=1.0):
         sigma = np.random.uniform(min_sigma, max_sigma)
         spec = skimage.filters.gaussian(spec, sigma=sigma, multichannel=False)
         return spec
@@ -142,7 +125,7 @@ class DataGenerator():
     # fade the spectrogram (smaller factors and larger min_vals lead to more fading);
     # defaults don't have a big visible effect but do fade values a little, and it's
     # important to preserve very faint spectrograms
-    def _fade(self, spec, min_factor=0.8, max_factor=0.99, min_val=0.06):
+    def _fade(self, spec, min_factor=0.6, max_factor=0.99, min_val=0.06):
         factor = np.random.uniform(min_factor, max_factor)
         spec *= factor
         spec[spec < min_val] = 0 # clear values near zero
@@ -151,7 +134,23 @@ class DataGenerator():
         return spec
                 
     # perform a random horizontal shift of the spectrogram
-    def _shift_horizontal(self, spec, max_shift_left, max_shift_right):
+    def _shift_horizontal(self, spec):
+        # detect left-shifted spectrograms, so we don't shift further left
+        left_part = spec[:self.spec_height, :10]
+        num_pixels = (left_part > 0.05).sum()
+        if num_pixels > 300:
+            max_shift_left = 0
+        else:
+            max_shift_left = MAX_SHIFT
+        
+        # detect right-shifted spectrograms, so we don't shift further right
+        right_part = spec[:self.spec_height, constants.SPEC_WIDTH - 10:]
+        num_pixels = (right_part > 0.05).sum()
+        if num_pixels > 300:
+            max_shift_right = 0
+        else:
+            max_shift_right = MAX_SHIFT
+
         if max_shift_left == 0 and max_shift_right == 0:
             return spec
         
@@ -160,6 +159,8 @@ class DataGenerator():
         return spec
 
     # multiply by random pixels (larger variances lead to more speckling)
-    def _speckle(self, spec, variance=0.01):
-        spec = skimage.util.random_noise(spec, mode='speckle', seed=self.seed, var=variance, clip=True)
+    def _speckle(self, spec):
+        index = random.randint(0, CACHE_LEN - 1)
+        spec = spec + spec * self.speckle[index]
+        spec = spec.clip(0, 1)
         return spec
