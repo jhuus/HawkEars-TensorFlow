25821468047bfc02dbfc43d8f60b7f6a2926ceb1
Jan Huus <75086432+jhuus@users.noreply.github.com>
2022-06-19 15:18:35 -0400

    Changes to noise handling

--- core/data_generator.py
+++ core/data_generator.py
@@ -1,6 +1,4 @@
 # Select and shuffle a random subset of available data, and apply data augmentation techniques.
-# I tried adding a separate thread to fill a queue of unzipped spectrograms,
-# but it didn't actually improve performance.
 
 import random
 import sys
@@ -14,7 +12,7 @@ from core import constants
 from core import util
 from core import plot
 
-PROB_MERGE = 0.2   # probability of merging to train multi-label support
+PROB_MERGE = 0.20  # probability of merging to train multi-label support
 PROB_AUG = 0.55    # probability of augmentation
 CACHE_LEN = 1000   # cache this many noise specs for performance
 
@@ -25,15 +23,17 @@ SPECKLE_VARIANCE = .009
 # relative frequencies of the augmentation types
 BLUR_INDEX = 0      # so FREQS[0] is relative frequency of blur
 FADE_INDEX = 1
-LOW_INDEX = 2
-NOISE_INDEX = 3
-SHIFT_INDEX = 4
-SPECKLE_INDEX = 5
+WHITE_NOISE_INDEX = 2
+PINK_NOISE_INDEX = 3
+REAL_NOISE_INDEX = 4
+SHIFT_INDEX = 5
+SPECKLE_INDEX = 6
 
-FREQS = [0.25, 0.5, 0.2, 1.0, 0.5, 0.9]
+FREQS = [0.25, 0.5, 1.0, 1.0, 0.5, 0.5, 0.9]
 
 class DataGenerator():
-    def __init__(self, x_train, y_train, seed=None, augmentation=True, binary_classifier=False, multilabel=False):
+    def __init__(self, db, x_train, y_train, seed=None, augmentation=True, binary_classifier=False, multilabel=False):
+        self.audio = audio.Audio()
         self.x_train = x_train
         self.y_train = y_train
         self.seed = seed
@@ -57,22 +57,21 @@ class DataGenerator():
             for i in range(1, SPECKLE_INDEX + 1):
                 self.probs[i] = self.probs[i - 1] + probs[i]
 
-            # self.local_vars is used in _add_low_noise;
-            # use exponentially more noise in the low frequencies
-            self.local_vars = np.zeros((self.spec_height, constants.SPEC_WIDTH, 1))
-            for row in range(0, self.spec_height):
-                max_val = ((self.spec_height - row) ** 4 / self.spec_height ** 4) / 70
-                for col in range(0, constants.SPEC_WIDTH):
-                    self.local_vars[row, col, 0] = np.random.uniform(0.001, max_val)
-
-            # creating these here instead of during augmentation saves a lot of time
-            self.noise = np.zeros((CACHE_LEN, self.spec_height, constants.SPEC_WIDTH, 1))
+            # create some white noise
+            self.white_noise = np.zeros((CACHE_LEN, self.spec_height, constants.SPEC_WIDTH, 1))
             for i in range(CACHE_LEN):
-                self.noise[i] = skimage.util.random_noise(self.noise[i], mode='gaussian', seed=self.seed, var=NOISE_VARIANCE, clip=True)
+                self.white_noise[i] = skimage.util.random_noise(self.white_noise[i], mode='gaussian', seed=self.seed, var=NOISE_VARIANCE, clip=True)
 
-            self.low_noise = np.zeros((CACHE_LEN, self.spec_height, constants.SPEC_WIDTH, 1))
+            # create some pink noise
+            self.pink_noise = np.zeros((CACHE_LEN, self.spec_height, constants.SPEC_WIDTH, 1))
             for i in range(CACHE_LEN):
-                self.low_noise[i] = skimage.util.random_noise(self.low_noise[i], mode='localvar', seed=self.seed, local_vars=self.local_vars, clip=True)
+                self.pink_noise[i] = self.audio.pink_noise() + self.white_noise[i]
+
+            # get some noise spectrograms from the database
+            results = db.get_spectrograms_by_name('Denoiser')
+            self.real_noise = np.zeros((len(results), constants.SPEC_HEIGHT, constants.SPEC_WIDTH, 1))
+            for i in range(len(self.real_noise)):
+                self.real_noise[i] = util.expand_spectrogram(results[i][0])
 
             self.speckle = np.zeros((CACHE_LEN, self.spec_height, constants.SPEC_WIDTH, 1))
             for i in range(CACHE_LEN):
@@ -86,32 +85,27 @@ class DataGenerator():
             label = self.y_train[id].astype(np.float32)
             
             if self.multilabel and not self.binary_classifier:
-                prob = np.random.uniform(0, 1)
+                prob = random.uniform(0, 1)
 
                 if prob < PROB_MERGE:
-                    index = random.randint(0, len(self.indices) - 1)
-                    other_id = self.indices[index]
-                    if id != other_id and not np.array_equal(self.y_train[id], self.y_train[other_id]):
-                        # merge two specs from different classes to train multi-label support
-                        other_spec = util.expand_spectrogram(self.x_train[other_id])
-                        spec += other_spec
-                        spec = spec.clip(0, 1)
-                        label += self.y_train[other_id].astype(np.float32)
+                    spec, label = self.merge_specs(spec, label)
 
             if self.augmentation:
-                prob = np.random.uniform(0, 1)
+                prob = random.uniform(0, 1)
                 if prob < PROB_AUG:
-                    prob = np.random.uniform(0, 1)
+                    prob = random.uniform(0, 1)
 
                     # it's very important to check these in this order
                     if prob < self.probs[BLUR_INDEX]:
                         spec = self._blur(spec)
                     elif prob < self.probs[FADE_INDEX]:
                         spec = self._fade(spec)
-                    elif prob < self.probs[LOW_INDEX]:
-                        spec = self._add_low_noise(spec)
-                    elif prob < self.probs[NOISE_INDEX]:
-                        spec = self._add_noise(spec)
+                    elif prob < self.probs[WHITE_NOISE_INDEX]:
+                        spec = self._add_white_noise(spec)
+                    elif prob < self.probs[PINK_NOISE_INDEX]:
+                        spec = self._add_pink_noise(spec)
+                    elif prob < self.probs[REAL_NOISE_INDEX]:
+                        spec = self._add_real_noise(spec)
                     elif prob < self.probs[SHIFT_INDEX]:
                         spec = self._shift_horizontal(spec)
                     else:
@@ -119,31 +113,58 @@ class DataGenerator():
 
             yield (spec.astype(np.float32), label)
     
-    # add low frequency noise to the spectrogram
-    def _add_low_noise(self, spec):
+    # pick a random spectrogram and merge it with the given one
+    def merge_specs(self, spec, label):
+        index = random.randint(0, len(self.indices) - 1)
+        other_id = self.indices[index]
+        other_spec = util.expand_spectrogram(self.x_train[other_id])
+        spec += other_spec
+        spec = spec.clip(0, 1)
+        label += self.y_train[other_id].astype(np.float32)
+        return spec, label
+
+    # add white noise to the spectrogram
+    def _add_white_noise(self, spec):
         index = random.randint(0, CACHE_LEN - 1)
-        spec += self.low_noise[index]
+        spec += self.white_noise[index]
         spec = spec.clip(0, 1)
         return spec
 
-    # add random noise to the spectrogram
-    def _add_noise(self, spec):
+    # add low frequency noise to the spectrogram
+    def _add_pink_noise(self, spec):
         index = random.randint(0, CACHE_LEN - 1)
-        spec += self.noise[index]
+        noise_mult = random.uniform(1.0, 3.0) # > 0 causes original spectrogram to fade accordingly
+        spec += self.pink_noise[index] * noise_mult
+        spec /= np.max(spec)
+        spec = spec.clip(0, 1)
+        return spec
+
+    # add real noise to the spectrogram
+    def _add_real_noise(self, spec):
+        index = random.randint(0, len(self.real_noise) - 1)
+        noise_mult = random.uniform(0.3, 0.85)
+        spec += self.real_noise[index] * noise_mult
+        spec /= np.max(spec)
         spec = spec.clip(0, 1)
         return spec
 
     # blur the spectrogram (larger values of sigma lead to more blurring)
     def _blur(self, spec, min_sigma=0.1, max_sigma=1.0):
-        sigma = np.random.uniform(min_sigma, max_sigma)
+        sigma = random.uniform(min_sigma, max_sigma)
         spec = skimage.filters.gaussian(spec, sigma=sigma, multichannel=False)
+
+        # renormalize to [0, 1]
+        max = spec.max()
+        if max > 0:
+            spec = spec / max
+
         return spec
         
     # fade the spectrogram (smaller factors and larger min_vals lead to more fading);
     # defaults don't have a big visible effect but do fade values a little, and it's
     # important to preserve very faint spectrograms
     def _fade(self, spec, min_factor=0.6, max_factor=0.99, min_val=0.06):
-        factor = np.random.uniform(min_factor, max_factor)
+        factor = random.uniform(min_factor, max_factor)
         spec *= factor
         spec[spec < min_val] = 0 # clear values near zero
         spec *= 1/factor # rescale so max = 1
