d6a731041097002ea5a0fcc1c8f8a36a63af1d72
Jan Huus <jhuus1@gmail.com>
2022-09-07 09:35:35 -0400

    minor changes

--- tools/delsd.py
+++ tools/delsd.py
@@ -1,10 +1,13 @@
-# Delete spectrograms from the database. Get the list from discards.txt, where
-# each line has the format "filename-offset", e.g. "XC10503-27.0" to identify the spectrogram.
+# Delete spectrograms from the database. Input is either a text file or a directory of images.
+# In a text file, each line has the format "filename-offset", e.g. "XC10503-27.0" to identify the spectrogram.
+# In a directory of images, each image name must have that format.
 
 import argparse
 import inspect
 import os
 import sys
+
+from pathlib import Path
 
 # this is necessary before importing from a peer directory
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -20,10 +23,12 @@ def fatal_error(msg):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', type=str, default='training', help='Database name. Default = training')
+parser.add_argument('-i', type=str, default='discards.txt', help='Input text file or directory name. Default = discards.txt')
 parser.add_argument('-s', type=str, default='', help='Species name')
 args = parser.parse_args()
 
 db = database.Database(filename=f'../data/{args.f}.db')
+input = args.i
 
 # need to get recording by species in case same file name is used for different species
 species_name = args.s
@@ -33,7 +38,17 @@ for result in results:
     recording_id, file_name, _ = result
     recording_dict[file_name[:-4]] = recording_id
 
-spec_names = util.get_file_lines('discards.txt')
+if Path(input).is_file():
+    spec_names = util.get_file_lines(input)
+else:
+    temp = os.listdir(input)
+    spec_names = []
+    for file_name in temp:
+        if os.path.isfile(os.path.join(input, file_name)):
+            base, ext = os.path.splitext(file_name)
+            if ext == '.png':
+                spec_names.append(base)
+
 for spec_name in spec_names:
     index = spec_name.rfind('-')
     if index == -1:
@@ -46,7 +61,7 @@ for spec_name in spec_names:
         recording_id = recording_dict[recording_name]
     else:
         fatal_error(f'recording not found: {recording_name}')
-        
+
     result = db.get_spectrogram(recording_id, offset)
     if result is None:
         print(f'spectrogram not found: {recording_name}-{offset}')
