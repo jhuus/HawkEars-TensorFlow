6419a55d710a1b53314aebab8726b5fb4c7e5fbf
Jan Huus <jhuus1@gmail.com>
2022-09-29 11:25:05 -0400

    minor updates

--- den_train.py
+++ den_train.py
@@ -21,12 +21,14 @@ from core import plot
 from core import util
 
 from model import mirnet
+from model import model_checkpoint
 
 BASE_LR = .0008
 BATCH_SIZE = 32
 CACHE_LEN = 1000 # cache this many noise specs for performance
 NOISE_VARIANCE = 0.0015 # larger variances lead to more noise
 NUM_EPOCHS = 6
+SAVE_EPOCH = 6 # save a ckpt starting at this epoch
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # 1 = no info, 2 = no warnings, 3 = no errors
 os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
@@ -58,29 +60,26 @@ class DataGenerator():
         for i, id in enumerate(self.indices):
             spec = self.x_train[id]
             if random.uniform(0, 1) < 0.4:
-                noisy_spec = self.add_low_noise(np.copy(spec))
+                spec, noisy_spec = self.add_noise(spec, self.low_noise)
             else:
-                noisy_spec = self.add_real_noise(np.copy(spec))
+                spec, noisy_spec = self.add_noise(spec, self.real_noise)
 
             yield (noisy_spec.astype(np.float32), spec.astype(np.float32))
 
-    # add artificial low frequency noise to the spectrogram
-    def add_low_noise(self, spec):
+    # return spec with a little noise added and noisy_spec with a lot of noise added
+    def add_noise(self, spec, noise):
+        noisy_spec = np.copy(spec)
         index = random.randint(0, CACHE_LEN - 1)
-        noise_mult = random.uniform(0.3, 0.85)
-        spec += self.low_noise[index] * noise_mult
-        spec /= np.max(spec)
-        spec = spec.clip(0, 1)
-        return spec
 
-    # add real noise to the spectrogram
-    def add_real_noise(self, spec):
-        index = random.randint(0, len(self.real_noise) - 1)
-        noise_mult = random.uniform(0.2, 0.7)
-        spec += self.real_noise[index] * noise_mult
+        noisy_spec += noise[index]
+        noisy_spec /= np.max(noisy_spec)
+        noisy_spec = noisy_spec.clip(0, 1)
+
+        spec += noise[index] * random.uniform(0, 0.3)
         spec /= np.max(spec)
         spec = spec.clip(0, 1)
-        return spec
+
+        return spec, noisy_spec
 
 # heuristic noise removal so clean spectrograms are extra clean;
 # for each frequency, subtract a multiple of the average amplitude
@@ -119,6 +118,9 @@ def get_spectrograms(db, name):
 # main entry point
 start_time = time.time()
 
+# mixed precision doesn't work well here
+# keras.mixed_precision.set_global_policy("mixed_float16")
+
 # define and compile the model
 model = mirnet.mirnet_model(input_shape=[cfg.spec_height, cfg.spec_width, 1])
 opt = keras.optimizers.Adam(learning_rate = cos_lr_schedule(0))
@@ -147,9 +149,10 @@ train_ds = tf.data.Dataset.from_generator(
 train_ds = train_ds.batch(BATCH_SIZE)
 
 checkpoint_path = "data/denoiser"
-cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=False, verbose=0, save_best_only=False)
+model_checkpoint_callback = model_checkpoint.ModelCheckpoint(checkpoint_path, SAVE_EPOCH, 0, copy_ckpt=True, save_best_only=False)
+
 lr_scheduler = keras.callbacks.LearningRateScheduler(cos_lr_schedule)
-callbacks = [cp_callback, lr_scheduler]
+callbacks = [model_checkpoint_callback, lr_scheduler]
 model.fit(train_ds, epochs=NUM_EPOCHS, validation_data=None, shuffle=True, callbacks=callbacks)
 
 elapsed = time.time() - start_time
