1e076892e5814402641792060446b6d2e4792a82
jhuus <jhuus1@gmail.com>
2023-01-03 13:02:12 -0500

    clean up logging code

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ from model import efficientnet_v2
 class Trainer:
     def __init__(self):
         self.db = database.Database(f'data/{cfg.training_db}.db')
-        self.classes = util.get_class_list()
+        self.classes = util.get_class_list(cfg.classes_file)
         self.init()
 
     # create a plot and save it to the output directory
@@ -71,10 +71,10 @@ class Trainer:
 
             opt = keras.optimizers.Adam(learning_rate = cos_lr_schedule(0))
             if cfg.multi_label:
-                loss = keras.losses.BinaryCrossentropy(label_smoothing = 0.13)
+                loss = keras.losses.BinaryCrossentropy(label_smoothing = cfg.label_smoothing)
                 model.compile(loss=loss, optimizer=opt)
             else:
-                loss = keras.losses.CategoricalCrossentropy(label_smoothing = 0.13)
+                loss = keras.losses.CategoricalCrossentropy(label_smoothing = cfg.label_smoothing)
                 model.compile(loss=loss, optimizer=opt, metrics='accuracy')
 
         # create output directory
@@ -82,12 +82,13 @@ class Trainer:
         if not os.path.exists(dir):
             os.makedirs(dir)
 
-        # output text and graphical descriptions of the model
-        with open(f'{dir}/table.txt','w') as text_output:
-            model.summary(print_fn=lambda x: text_output.write(x + '\n'))
+        if cfg.verbosity > 0:
+            # output text and graphical descriptions of the model
+            with open(f'{dir}/table.txt','w') as text_output:
+                model.summary(print_fn=lambda x: text_output.write(x + '\n'))
 
-        if cfg.verbosity == 3:
-            keras.utils.plot_model(model, show_shapes=True, to_file=f'{dir}/graphic.png')
+            if cfg.verbosity >= 2:
+                keras.utils.plot_model(model, show_shapes=True, to_file=f'{dir}/graphic.png')
 
         # initialize callbacks
         lr_scheduler = keras.callbacks.LearningRateScheduler(cos_lr_schedule)
@@ -117,40 +118,29 @@ class Trainer:
             shuffle = False, callbacks = callbacks, class_weight = self._get_class_weight())
         elapsed = time.time() - start_time
 
-        if cfg.verbosity >= 2:
-            # output loss graph
-            if len(self.x_test) > 0:
-                self.plot_results(dir, history, 'loss', 'val_loss')
-            else:
-                self.plot_results(dir, history, 'loss')
-
-            if not cfg.multi_label:
-                # output accuracy graph
-                training_accuracy = history.history["accuracy"][-1]
-                if len(self.x_test) > 0:
-                    self.plot_results(dir, history, 'accuracy', 'val_accuracy')
-                    scores = model.evaluate(self.x_test, self.y_test)
-                    test_accuracy = scores[1]
-                else:
-                    self.plot_results(dir, history, 'accuracy')
-
-            if len(self.x_test) > 0:
-                # report on misidentified test spectrograms
-                predictions = model.predict(self.x_test)
-                self.analyze_predictions(predictions)
-
         if cfg.verbosity > 0:
             with open(f'{dir}/summary.txt','w') as text_output:
                 text_output.write(f'EfficientNetV2 config: {cfg.eff_config}\n')
                 text_output.write(f'Batch size: {cfg.batch_size}\n')
                 text_output.write(f'Epochs: {cfg.num_epochs}\n')
 
-                text_output.write(f"Training loss: {history.history['loss'][-1]:.3f}\n")
+                if 'loss' in history.history:
+                    text_output.write(f"Training loss: {history.history['loss'][-1]:.3f}\n")
 
-                if not cfg.multi_label:
+                if not cfg.multi_label and 'accuracy' in history.history:
+                    # output accuracy graph
+                    training_accuracy = history.history['accuracy'][-1]
                     text_output.write(f'Training accuracy: {training_accuracy:.3f}\n')
 
-                if cfg.verbosity >= 2 and len(self.x_test) > 0:
+                    if len(self.x_test) > 0:
+                        self.plot_results(dir, history, 'accuracy', 'val_accuracy')
+                        scores = model.evaluate(self.x_test, self.y_test)
+                        test_accuracy = scores[1]
+                    else:
+                        self.plot_results(dir, history, 'accuracy')
+
+
+                if cfg.verbosity >= 1 and len(self.x_test) > 0:
                     text_output.write(f'Test loss: {scores[0]:.3f}\n')
                     text_output.write(f'Final test accuracy: {test_accuracy:.3f}\n')
                     text_output.write(f'Best test accuracy: {model_checkpoint_callback.best_val_accuracy:.4f}\n')
@@ -159,6 +149,12 @@ class Trainer:
                 seconds = int(elapsed) % 60
                 text_output.write(f'Elapsed time for training = {minutes}m {seconds}s\n')
 
+            if cfg.verbosity >= 1 and 'loss' in history.history:
+                # output loss graph
+                if len(self.x_test) > 0:
+                    self.plot_results(dir, history, 'loss', 'val_loss')
+                else:
+                    self.plot_results(dir, history, 'loss')
 
             if len(self.x_test) > 0:
                 print(f'Best test accuracy: {model_checkpoint_callback.best_val_accuracy:.4f}\n')
@@ -166,84 +162,6 @@ class Trainer:
             print(f'Elapsed time for training = {minutes}m {seconds}s\n')
 
         return model_checkpoint_callback.best_val_accuracy
-
-    # find and report on incorrect predictions;
-    # always generate summary/stats.csv, but output misident/*.png only if verbosity >= 2;
-    # this is based on the last epoch, which may not be the best saved model
-    def analyze_predictions(self, predictions):
-        class ClassInfo:
-            def __init__(self):
-                self.spec_count = 0
-                self.true_pos = 0
-                self.false_pos = 0
-                self.false_neg = 0
-
-        misident_dir = 'misident'
-        if os.path.exists(misident_dir):
-            shutil.rmtree(misident_dir) # ensure we start with an empty folder
-
-        os.makedirs(misident_dir)
-
-        # collect data per class and output images if requested
-        classes = {}
-        for i in range(len(predictions)):
-            actual_index = np.argmax(self.y_test[i])
-            actual_name = self.classes[actual_index]
-
-            predicted_index = np.argmax(predictions[i])
-            predicted_name = self.classes[predicted_index]
-
-            if actual_name in classes:
-                actual_class_info = classes[actual_name]
-            else:
-                actual_class_info = ClassInfo()
-
-                classes[actual_name] = actual_class_info
-
-            if predicted_name in classes:
-                predicted_class_info = classes[predicted_name]
-            else:
-                predicted_class_info = ClassInfo()
-                classes[predicted_name] = predicted_class_info
-
-            actual_class_info.spec_count += 1
-            if predicted_index == actual_index:
-                actual_class_info.true_pos += 1
-            else:
-                actual_class_info.false_neg += 1
-                predicted_class_info.false_pos += 1
-
-                if cfg.verbosity >= 2:
-                    if i in self.spec_file_name.keys():
-                        suffix = self.spec_file_name[i]
-                    else:
-                        suffix = i
-
-                    spec = self.x_test[i].reshape(self.x_test[i].shape[0], self.x_test[i].shape[1])
-                    plot.plot_spec(spec, f'{misident_dir}/{actual_name}_{predicted_name}_{suffix}.png')
-
-        # output stats.csv containing data per class
-        stats = 'class,count,TP,FP,FN,FP+FN,precision,recall,average\n'
-        for class_name in sorted(classes):
-            count = classes[class_name].spec_count
-            tp = classes[class_name].true_pos
-            fp = classes[class_name].false_pos
-            fn = classes[class_name].false_neg
-
-            if tp + fp == 0:
-                precision = 0
-            else:
-                precision = tp / (tp + fp)
-
-            if tp + fn == 0:
-                recall = 0
-            else:
-                recall = tp / (tp + fn)
-
-            stats += f'{class_name},{count},{tp:.3f},{fp:.3f},{fn:.3f},{fp + fn:.3f},{precision:.3f},{recall:.3f},{(precision+recall)/2:.3f}\n'
-
-        with open(f'summary/stats.csv','w') as text_output:
-            text_output.write(stats)
 
     # given the total number of spectrograms in a class, return a dict of randomly selected
     # indices to use for testing (indices not in the list are used for training)
@@ -253,7 +171,7 @@ class Trainer:
         test_indices = {}
         while len(test_indices.keys()) < num_test:
             index = random.randint(0, total - 1)
-            if index not in test_indices.keys():
+            if index not in test_indices:
                 test_indices[index] = 1
 
         return test_indices
@@ -264,6 +182,10 @@ class Trainer:
         sum = 0
         for class_name in self.classes:
             count = float(self.db.get_spectrogram_count(class_name))
+            if count == 0:
+                print(f'Error. No spectrograms found for "{class_name}"')
+                quit()
+
             num_specs[class_name] = count
             sum += count
 
@@ -301,19 +223,6 @@ class Trainer:
             test_total += test_count
             train_total += train_count
 
-        if len(cfg.validation_db) > 0:
-            # when we just use a portion of the training data for testing/validation, it ends up being highly
-            # correlated with the training data, so the validation percentage is artificially high and it's
-            # difficult to detect overfitting;
-            # adding separate test data from a validation database helps to counteract this;
-            # there can be multiple, which must be comma-separated
-            val_names = cfg.validation_db.split(',')
-            for val_name in val_names:
-                validation_db = database.Database(f'data/{val_name}.db')
-                num_validation_specs = 0
-                for class_name in self.classes:
-                    test_total += validation_db.get_spectrogram_count(class_name)
-
         print(f'# training samples: {train_total}, # test samples: {test_total}')
 
         # initialize arrays
@@ -338,7 +247,7 @@ class Trainer:
             for r in results:
                 results2 = self.db.get_spectrogram('RecordingID', r.id)
                 for r2 in results2:
-                    if spec_index in self.test_indices[i].keys():
+                    if spec_index in self.test_indices[i]:
                         # test spectrograms are expanded here
                         self.spec_file_name[test_index] = f'{r.filename}-{r2.offset}' # will be used in names of files written to misident folder
                         self.x_test[test_index] = util.expand_spectrogram(r2.value, low_noise_detector=cfg.low_noise_detector)
@@ -352,18 +261,6 @@ class Trainer:
                         train_index += 1
 
                     spec_index += 1
-
-        if len(cfg.validation_db) > 0:
-            # append test data from the validation database(s)
-            val_names = cfg.validation_db.split(',')
-            for val_name in val_names:
-                validation_db = database.Database(f'data/{val_name}.db')
-                for i in range(len(self.classes)):
-                    results = validation_db.get_spectrogram_by_subcat_name(self.classes[i])
-                    for r in results:
-                        self.x_test[test_index] = util.expand_spectrogram(r.value, low_noise_detector=cfg.low_noise_detector)
-                        self.y_test[test_index][i] = 1
-                        test_index += 1
 
 # learning rate schedule with cosine decay
 def cos_lr_schedule(epoch):
@@ -387,8 +284,7 @@ if __name__ == '__main__':
     parser.add_argument('-r', type=float, default=cfg.base_lr, help=f'Base learning rate. Default = {cfg.base_lr}.')
     parser.add_argument('-t', type=float, default=cfg.test_portion, help=f'Test portion. Default = {cfg.test_portion}')
     parser.add_argument('-u', type=int, default=1, help='1 = If 1, train a multi-label classifier. Default = 1.')
-    parser.add_argument('-v', type=int, default=1, help='Verbosity (0-2, 0 omits output graphs, 2 plots misidentified test spectrograms, 3 adds graph of model). Default = 1.')
-    parser.add_argument('-x', type=str, default=cfg.validation_db, help='Name(s) of extra validation databases. "abc" means load "abc.db". "abc,def" means load both databases for validation. Default = "".')
+    parser.add_argument('-v', type=int, default=1, help='Verbosity (0-2, 0 is minimal, 2 includes graph of model). Default = 1.')
     parser.add_argument('-y', type=int, default=0, help='If y = 1, extract spectrograms for low-noise detector. Default = 0.')
     parser.add_argument('-z', type=int, default=cfg.seed, help=f'Integer seed for random number generators. Default = {cfg.seed}. If specified, other settings to increase repeatability will also be enabled, which slows down training.')
 
@@ -403,14 +299,13 @@ if __name__ == '__main__':
     cfg.test_portion = args.t
     cfg.multi_label = (args.u == 1)
     cfg.verbosity = args.v
-    cfg.validation_db = args.x
     cfg.low_noise_detector = (args.y == 1)
     cfg.seed = args.z
 
     if cfg.seed != None:
         # these settings make results more reproducible, which is very useful when tuning parameters
         os.environ['PYTHONHASHSEED'] = str(cfg.seed)
-        #os.environ['TF_CUDNN_DETERMINISTIC'] = '1' # uncomment this for more determinism, but much slower
+        # tf.config.experimental.enable_op_determinism() # uncomment this for more determinism, but MUCH slower
         random.seed(cfg.seed)
         np.random.seed(cfg.seed)
         tf.random.set_seed(cfg.seed)
