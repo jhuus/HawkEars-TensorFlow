447b509524d9b5b4fc30df3915902829da7f9901
jhuus <jhuus1@gmail.com>
2023-01-28 09:43:34 -0500

    Overhaul data augmentation logic and parameters

--- core/config.py
+++ core/config.py
@@ -1,32 +1,38 @@
 # basic audio
 segment_len = 3         # spectrogram duration in seconds
 sampling_rate = 40960
-hop_length = 320
-win_length = 2048
+hop_length = 320        # FFT parameter
+win_length = 2048       # FFT parameter
 spec_height = 128       # spectrogram height
 spec_width = 384        # spectrogram width (3 * 128)
 min_audio_freq = 200
 max_audio_freq = 10500
 mel_scale = True
-spec_exponent = .75      # raise spectrogram values to this exponent (brings out faint sounds)
+mel_amplitude_adjustment = True # correct for amplitude distortion caused by mel scaling
+spec_exponent = .68      # raise spectrogram values to this exponent (brings out faint sounds)
 spec_block_seconds = 240 # max seconds of spectrogram to create at a time (limited by GPU memory)
-high_pass_filter = True
-mel_amplitude_adjustment = True
 
 # training
-base_lr = .006          # base learning rate
+seed = 1
+base_lr = .003          # base learning rate
+cos_decay_pad = 0       # larger values reduce cosine decay of learning rate
 batch_size = 32
-eff_config = 'a3'       # EfficientNet_v2 configuration to use
-num_epochs = 18
-ckpt_min_epochs = 15    # min epochs before saving checkpoint
+eff_config = 'a3'       # EfficientNet_v2 configuration to use ('r' for ResNest)
+eff_dropout = 0.2       # dropout rate for EfficientNext_V2 output block
+eff_drop_connect = 0.4  # dropout rate for EfficientNext_V2 non-output blocks
+resnest_stages = 2      # if eff_config = 'r'
+resnest_blocks = [2,2]
+num_epochs = 1
+ckpt_min_epochs =  5    # min epochs before saving checkpoint
 ckpt_min_val_accuracy = 0 # min validation accuracy before saving checkpoint
 copy_ckpt = True        # save a copy of each checkpoint
-label_smoothing = 0.13
+label_smoothing = 0.15
+
 load_saved_model = False
-mixed_precision = True  # trains much faster if true
+mixed_precision = True  # mixed precision trains faster with large models, but slower with tiny models
 multi_label = True
 save_best_only = False
-seed = None
+deterministic = False    # may reduce variance a bit, but still not deterministic
 test_portion = 0
 frequency_db = 'frequency' # eBird barchart data, i.e. species report frequencies
 training_db = 'training' # name of training database
@@ -34,31 +40,20 @@ verbosity = 1            # 0 omits output graphs, 2 adds plots of misidentified 
 
 # data augmentation
 augmentation = True
-prob_aug = 0.45         # probability of augmenting a given spectrogram
-prob_merge = 0.25       # probability of merging to train multi-label support
-max_shift = 5           # max pixels for horizontal shift
-noise_variance = 0.0015 # larger variances lead to more noise
+prob_merge = 0.35        # probability of merging to train multi-label support
+prob_aug = 0.5           # probability of augmenting after merge and before fade
+white_noise_weight = 1.0 # weights give relative probability of each augmentation type
+shift_weight = 0.02
+speckle_weight = 0.04
+max_shift = 5            # max pixels for horizontal shift
+white_noise_variance = 0.001 # larger variances lead to more noise
 speckle_variance = .009
-noise_min = 0.1
-noise_max = 0.5
-min_fade_factor=0.6
-max_fade_factor=0.99
-min_fade_val=0.05
-
-blur_freq = 0.25        # following are relative frequencies of each augmentation
-fade_freq = 0.5
-white_noise_freq = 0.1
-pink_noise_freq = 0.1
-real_noise_freq = 0.8
-shift_freq = 0.5
-speckle_freq = 0.9
-
-min_mult = 0.8
-max_mult = 1.0
+min_fade = 0.1           # multiply values by a random float in [min_fade, max_fade]
+max_fade = 1.0
 
 # analysis / inference
 min_prob = 0.85              # minimum confidence level
-use_banding_codes = False    # use banding codes instead of species names in labels
+use_banding_codes = True     # use banding codes instead of species names in labels
 check_adjacent = True        # omit label unless adjacent segment matches
 adjacent_prob_factor = 0.65  # when checking if adjacent segment matches species, use self.min_prob times this
 reset_model_counter = 10     # in analysis, reset the model every n loops to avoid running out of GPU memory
@@ -72,15 +67,13 @@ file_date_regex_group = 1    # use group at offset 1
 # frequency across all weeks (i.e. maximum portion of checklists that include the species).
 # If the maximum frequency for a species in a soundalike group is <= soundalike_cutoff,
 # it is replaced by the species with the highest frequency > soundalike_cutoff in the group.
-# For instance, if a Pacific Wren is ID'd in a county where it's never been seen,
-# but Winter Wrens are common there, it will be reported as a Winter Wren.
+# For instance, if a Mountain Chickadee is ID'd in a county where it's never been seen,
+# but Black-capped Chickadees are common there, it will be reported as a Black-capped Chickadee.
 soundalike_cutoff = .005
-soundalikes = [['Pacific Wren', 'Winter Wren'],
-               ['Black-capped Chickadee', 'Boreal Chickadee', 'Mountain Chickadee']]
+soundalikes = [['Black-capped Chickadee', 'Boreal Chickadee', 'Mountain Chickadee'],
+               ['Pacific Wren', 'Winter Wren']]
 
 # paths
 ignore_file = 'data/ignore.txt'     # classes listed in this file are ignored in analysis
 classes_file = 'data/classes.txt'   # list of classes used in training and analysis
 ckpt_path = 'data/ckpt'             # main model checkpoint
-bird_ckpt_path = 'data/is_bird_ckpt' # model checkpoint for bird detector
-low_noise_ckpt_path = 'data/is_low_noise_ckpt' # model checkpoint for low-frequency noise detector
