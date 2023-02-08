dcc67bfbb6f5bb5310ac5b45f98c48bd96677714
jhuus <jhuus1@gmail.com>
2023-02-08 09:08:10 -0500

    Simplify code a little

--- core/data_generator.py
+++ core/data_generator.py
@@ -15,18 +15,12 @@ from core import plot
 
 CACHE_LEN = 1000   # cache this many white noise spectrograms for performance
 
-# indexes of augmentation types
-WHITE_NOISE_INDEX = 0
-SHIFT_INDEX = 1
-SPECKLE_INDEX = 2
-
 class DataGenerator():
     def __init__(self, db, x_train, y_train, train_class):
         self.audio = audio.Audio()
         self.x_train = x_train
         self.y_train = y_train
         self.train_class = train_class
-        self.set_probabilities()
 
         self.indices = np.arange(y_train.shape[0])
         if cfg.augmentation:
@@ -38,17 +32,6 @@ class DataGenerator():
             self.speckle = np.zeros((CACHE_LEN, cfg.spec_height, cfg.spec_width, 1))
             for i in range(CACHE_LEN):
                 self.speckle[i] = self._get_white_noise(cfg.speckle_variance)
-
-    # get relative weights of augmentation types and convert to probability ranges in [0, 1]
-    def set_probabilities(self):
-        weights = np.array([cfg.white_noise_weight, cfg.shift_weight, cfg.speckle_weight])
-
-        sum = np.sum(weights)
-        probs = weights / sum
-        self.probs = np.zeros(SPECKLE_INDEX + 1)
-        self.probs[0] = probs[0]
-        for i in range(1, SPECKLE_INDEX + 1):
-            self.probs[i] = self.probs[i - 1] + probs[i]
 
     # this is called once per epoch to generate the spectrograms
     def __call__(self):
@@ -69,14 +52,10 @@ class DataGenerator():
                 prob = random.uniform(0, 1)
                 if prob < cfg.prob_aug:
                     prob = random.uniform(0, 1)
-
-                    # it's very important to check these in this order
-                    if prob < self.probs[WHITE_NOISE_INDEX]:
-                        spec = self._add_white_noise(spec)
-                    elif prob < self.probs[SHIFT_INDEX]:
-                        spec = self._shift_horizontal(spec)
-                    else:
+                    if prob < cfg.prob_speckle:
                         spec = self._speckle(spec)
+                    else:
+                        spec = self._add_white_noise(spec)
 
                     spec = self._normalize_spec(spec)
 
@@ -120,32 +99,6 @@ class DataGenerator():
         if max > 0:
             spec = spec / max
 
-        return spec
-
-    # perform a random horizontal shift of the spectrogram, using simple heuristics
-    # to avoid shifting the important part of the signal past the edge
-    def _shift_horizontal(self, spec):
-        # detect left-shifted spectrograms, so we don't shift further left
-        left_part = spec[:cfg.spec_height, :10]
-        num_pixels = (left_part > 0.05).sum()
-        if num_pixels > 300:
-            max_shift_left = 0
-        else:
-            max_shift_left = cfg.max_shift
-
-        # detect right-shifted spectrograms, so we don't shift further right
-        right_part = spec[:cfg.spec_height, cfg.spec_width - 10:]
-        num_pixels = (right_part > 0.05).sum()
-        if num_pixels > 300:
-            max_shift_right = 0
-        else:
-            max_shift_right = cfg.max_shift
-
-        if max_shift_left == 0 and max_shift_right == 0:
-            return spec
-
-        pixels = random.randint(-max_shift_left, max_shift_right)
-        spec = np.roll(spec, shift=pixels, axis=1)
         return spec
 
     # add a copy multiplied by random pixels (larger variances lead to more speckling)
