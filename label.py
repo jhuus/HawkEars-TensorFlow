9c107f0e33ea32903900bf4ff2c9d3fe9bba58b9
jhuus <jhuus1@gmail.com>
2023-02-13 15:56:13 -0500

    add support for species handlers

---
+++ label.py
@@ -0,0 +1,8 @@
+# Label class used by analyze.py and species_handlers.py.
+
+class Label:
+    def __init__(self, class_name, probability, start_time, end_time):
+        self.class_name = class_name
+        self.probability = probability
+        self.start_time = start_time
+        self.end_time = end_time
